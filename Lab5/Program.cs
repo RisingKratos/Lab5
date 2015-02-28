6231ff1812e7be4b83d55270929758febceec7c0
RisingKratos <risingkratos@gmail.com>
2015-02-28 12:18:11 +0600

    V

---
+++ Lab5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Serialize
+{
+    [Serializable]
+    public class Person
+    {
+        public Person(int age)
+        {
+            this.Age = age;
+        }
+        public Person()
+        {
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        private static void Serializing(Person sp)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(Person));
+
+            FileStream fs = new FileStream("t.txt", FileMode.OpenOrCreate);
+                    
+
+            x.Serialize(fs, sp);
+
+            fs.Close();
+
+            fs = new FileStream("t.txt", FileMode.Open);
+
+            Person p2 = x.Deserialize(fs) as Person;
+
+            fs.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Person s = new Person(18);
+            s.Name = "Muslim";
+            s.Surname = "Beibytuly";
+            Serializing(s);
+        }
+    }
+}
